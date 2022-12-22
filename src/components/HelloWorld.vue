ce014d8ebf09ff54bf20f956fbbf19fcf9b656a4
Alan Michelin <alan17pc@gmail.com>
2022-12-22 20:55:13 -0300

    app andando

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,71 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Conversor de temperatura</h1>
+    <div>
+      <div>
+        <label for="input_temp">Ingresar temperatura:</label>
+        <input type="number" id="input_temp" @input="calcular"/>
+      </div>
+      <div>
+        <label for="temp_f">Conv temperatura °F:</label>
+        <input type="number" :style="{color:colorTxt}" id="temp_f" readonly v-model="temperaturas.farenheit">
+      </div>
+      <div>
+        <label for="temp_k">Conv temperatura °K:</label>
+        <input type="number" :style="{color:colorTxt}" id="temp_k" readonly v-model="temperaturas.kelvin">
+      </div>
+    </div>
+    <div>
+      <h2>Respuestas</h2>
+      <ul style="text-align:left">
+        <li>a. @click</li>
+        <li>b. Crear un proyecto base Vue basado en Vue CLI </li>
+        <li>c. Los espacios adelante y atrás en una entrada de texto</li>
+        <li>d. Definir el path y el componente activo para esa ruta</li>
+        <li>e. La lógica para cambiar el estado global de la aplicación</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      temperaturas:{
+        farenheit: '',
+        kelvin: ''
+      },
+      colorTxt:'black'
+    }
+  },
+  methods:{
+  calcular(e){
+      let valorInput = parseInt(e.target.value)
+      this.temperaturas ={
+        farenheit: valorInput + 32,
+        kelvin: valorInput + 273.15
+      }
+      this.colorTxt = this.checkColor(valorInput)
+    },
+  checkColor(num){
+    let color = 'black'
+    if(!num){
+      return color;
+    }
+
+    if(num<=0){
+      color = 'blue'
+    }
+    else if(num>0 && num<15){
+      color = 'magenta'
+    }
+    else{
+      color= 'red'
+    }
+    return color;
+  }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
